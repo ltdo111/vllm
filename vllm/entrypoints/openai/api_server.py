aae86b3d96bbe46ec557468426b0a93bb3082157
lt <liutao491@huawei.com>
2025-06-21 15:40:50 +0800

    delete invalid restful api

--- vllm/entrypoints/openai/api_server.py
+++ vllm/entrypoints/openai/api_server.py
@@ -989,28 +989,7 @@ if envs.VLLM_ALLOW_EXPERT_LOAD_COLLECTING:
         expert_load = await engine_client(raw_request).get_expert_load()
         logger.info("Get expert load success!!!")
 
-        # server_info = {"vllm_config": str(raw_request.app.state.vllm_config)}
         return JSONResponse(content=expert_load)
-
-        # return Response(status_code=200, expert_load=expert_load)
-
-
-    @router.post("/stop_expert_distribution_record")
-    async def stop_expert_distribution_record(raw_request: Request):
-        """Stop recording the expert distribution."""
-        logger.info("Stopping expert distribution record...")
-        await engine_client(raw_request).stop_expert_distribution_record()
-        logger.info("Expert distribution record stopped.")
-        return Response(status_code=200)
-
-
-    @router.post("/dump_expert_distribution_record")
-    async def dump_expert_distribution_record(raw_request: Request):
-        """Dump expert distribution record."""
-        logger.info("Dumping expert distribution record...")
-        await engine_client(raw_request).dump_expert_distribution_record()
-        logger.info("Expert distribution record dumped.")
-        return Response(status_code=200)
 
 if envs.VLLM_ALLOW_RUNTIME_LORA_UPDATING:
     logger.warning(
